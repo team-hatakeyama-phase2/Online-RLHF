0c612a1854dd53538a48f2895c49fe9beaa1b2dc
Your Name <you@example.com>
2024-06-17 17:59:32 +0000

    add

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
-from transformers import AutoTokenizer, pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import uvicorn
 from accelerate import Accelerator
 import torch
+import deepspeed
 
 app = FastAPI()
 
@@ -12,13 +13,14 @@ pipe_kwargs = {
     "batch_size": 1,
 }
 
-reward_model = "/storage5/takagi/models/mistral_rm"
+#reward_model = "/storage5/takagi/models/mistral_rm"
+reward_model = "/storage5/takagi/models/swallow_mx_rm_lora"
 
 rm_tokenizer = AutoTokenizer.from_pretrained(reward_model)
 rm_pipe = pipeline(
     "sentiment-analysis",
     model=reward_model,
-    device="cuda",
+    device_map="auto",
     tokenizer=rm_tokenizer,
     model_kwargs={"torch_dtype": torch.bfloat16},
     truncation=True,
